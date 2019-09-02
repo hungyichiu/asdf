fd6a7d5efb55bae229ab981b4b98efe2812fc8bc
Chris Chiu <hungyichiu@gmail.com>
2019-09-02 16:31:29 +0800

    add Devise

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'hirb-unicode'
   gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+  gem 'hirb-unicode'
 
 end
 
--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,8 +1,9 @@
 class Candidate < ApplicationRecord
   validates :name, presence: true
-  validates :age, numericality: { greater_than: 40,less_than: 70 }
+  validates :age, numericality: { greater_than: 40}
   #validates(:name, {prensence: true})
   has_many :vote_logs, dependent: :destroy
+  has_many :users, through: :vote_logs
 end
 
 # rails g model Candidate ...
